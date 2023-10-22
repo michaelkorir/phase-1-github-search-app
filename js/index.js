81a1ac3416b30654ca17090c47ff8c8990a7054f
michael-korir <michaelkiptoo2000@gmail.com>
2023-10-22 21:44:38 +0300

    github search app

--- js/index.js
+++ js/index.js
@@ -1,44 +1,20 @@
-// const usersUrl=``
-// const reposUrl=``
-const userList=document.getElementById('user-list')
-let repoList=document.getElementById('repos-list')
-
-
-const form=document.querySelector('#github-form')
-form.addEventListener('submit',(e)=>{
+let form = document.getElementById("myForm");
+form.addEventListener('submit',function(e){
     e.preventDefault()
-    let username=e.target.search.value
-    console.log('searching github')
-    fetch(`https://api.github.com/search/users?q=${username}`)
-    .then(res=>res.json())
-    .then(data=>data.forEach(user=>{
+    let search =document.getElementById('search').value
+    let originalName= search.split(" ").join('')
 
-      let h5=document.createElement('h5')  
-      h5.innerText =`${user.login}`
-      let img=document.createElement('img')  
-      img.src=`{user.avatar}`
-      let link=document.createElement('a')
-      link.href=`link to their  profile`
+    document.getElementById("result").innerHTML=('')
+    
+    
+    fetch("https://api.github.com/users/" +originalName)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
 
-      
-      
-      //add event to the users to show more info about them
-      h5.addEventListener('click',(e)=>{
-        let username=e.target.value//get the username
-
-        //make a fetch request with the returned value
-        //display the returned results on the DOM
-        fetch(`https://api.github.com/users/${username}/repos`)
-        .then(res=>res.json())
-        .then(repos=>repos.forEach(repo=>{
-         let repo=document.createElement('li')
-         repo.innerText=`${repo.name}`
-
-        }))
-        repoList.append(repo)
-      });
-      userList.appendChild(h5,img,link)
+        document.getElementById('result').innerHTML=`
+        <a href="https://www.github.com/${originalName}"> <img src="${data.avatar_url}"/><a/>
+        `
     })
-          
-)
+
 })
